eab2c17be308fdd10d2ef45d4e7b01c06af078d5
efthimispegas <efthimispegas@gmail.com>
2021-03-22 17:25:12 +0200

    Wire categories list and header components to the dashboard component

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import CategoriesList from '../categoriesList/CategoriesList';
+import Header from '../common/header/Header';
 import Menu from '../common/menu/Menu';
 import './Dashboard.scss'
 
@@ -6,16 +8,8 @@ const Dashboard = () => (
   <div className="demo-dashboard-container">
     <Menu />
     <div className="demo-main-page-container">
-      <div className="demo-dashboard-header-container">
-        <h4 className="demo-dashboard-header-text">Κεντρική Σελίδα</h4>
-        <div className="demo-dashboard-searchbar"><p className="demo-dashboard-search-text">Αναζήτηση</p></div>
-      </div>
-      <div className="demo-dashboard-categories">
-        {/* Make reuseable component */}
-        <div className="demo-dashboard-item"><h6 className="demo-dashboard-item-text">Item 1</h6></div>
-        <div className="demo-dashboard-item"><h6 className="demo-dashboard-item-text">Item 2</h6></div>
-        <div className="demo-dashboard-item"><h6 className="demo-dashboard-item-text">Item 3</h6></div>
-      </div>
+      <Header />
+      <CategoriesList />
     </div>
   </div>
   );
